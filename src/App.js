a923ff334ccde456f3380fd09c291f487df49eae
Taoufik <ultrasarabic@gmail.com>
2022-12-04 12:40:57 +0100

    basic_filter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import "./app.css";
+import { users } from "./dummyData";
 function App() {
+  const [filter, setfilter] = useState("")
+
   return (
     <div>
-      Hello world!
+      <input type={"text"} placeholder="search" className="search" onChange={(e)=>setfilter(e.target.value)} />
+      <ul className="ul">
+        {users.filter(user=>user.username.toLowerCase().includes(filter.toLowerCase())).map((user)=>
+          <li className="li" key={user.id}>{user.username}</li>
+        )}
+      </ul>
     </div>
   );
 }
